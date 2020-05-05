26c2838be0c8aab6442ae98f122e40a8f30a771d
muyouking <343041958@qq.com>
2020-05-05 17:31:37 +0800

    Add files via upload
    大概架构

---
+++ KOC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 单个KOC信息
+/// </summary>
+public class KOC : MonoBehaviour
+{
+    public Sprite sprite;
+    public string simply;
+
+    public KOC(Sprite sprite,string simply)
+    {
+        this.sprite = sprite;
+        this.simply = simply;
+    }
+    public KOC()
+    {
+
+    }
+}
---
+++ KOCDirectory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class KOCDirectory : MonoBehaviour
+{
+    KOC OrgKoc; //原始KOC
+    List<KOC> KocDirectory;     //koc文件夹
+    List<KOC> More5;    //相似度大于0.5
+    List<KOC> Less5;    //相似度小于0.5
+
+    public KOCDirectory(List<KOC> KocDirectory)
+    {
+        this.KocDirectory = KocDirectory;
+        GetKocArray();
+    }
+
+    public void GetKocArray()
+    {
+        this.More5 = new List<KOC>();
+        this.Less5 = new List<KOC>();
+        foreach (var item in KocDirectory)
+        {
+            if ((item.simply)!="0")
+            {
+                More5.Add(item);
+
+            }
+            else
+            {
+                Less5.Add(item);
+            }
+        }
+
+    }
+    
+}
